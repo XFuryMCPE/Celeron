a671a9034ef8f4444d27579dc8f0c2277a92d198
Syntaxious <SynPlaysMC@gmail.com>
2016-04-08 17:07:58 +0100

    Update Main.php

--- src/Celeron/Main.php
+++ src/Celeron/Main.php
@@ -1,1 +1,12 @@
+<?php
+
+namespace Celeron;
+
+use pocketmine\plugin\Pluginbase;
+
+class Main extends PluginBase implements Listner{
+  public function onEnable(){
+    $this->getLogger()->info(TF::BLUE."Celeron connected");
+  }
+}
 
