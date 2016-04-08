043f1140f879dce84766f0dcd27756bc6f4f61d9
Syntaxious <SynPlaysMC@gmail.com>
2016-04-08 17:28:18 +0100

    Update Main.php

--- src/Celeron/Main.php
+++ src/Celeron/Main.php
@@ -2,11 +2,36 @@
 
 namespace Celeron;
 
-use pocketmine\plugin\Pluginbase;
+use pocketmine\event\Listener;
+use pocketmine\plugin\Plugin;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\utils\TextFormat;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\player\PlayerItemHeldEvent;
+use pocketmine\item\Item;
 
-class Main extends PluginBase implements Listner{
-  public function onEnable(){
-    $this->getLogger()->info(TF::BLUE."Celeron connected");
-  }
+class Main extends PluginBase implements Listener{
+
+    public function onEnable(){
+        $this->getServer()->info("Celeron initiated");
+        $this->getServer()->getPluginManager()->registerEvents($this,$this);
+    }
+
+    public function onDisable(){
+        $this->getLogger()->info("Celeron disabled");
+    }
+
+    public function onHurt(EntityDamageEvent $event){
+        $player = $event->getEntity();
+        if($event instanceof EntityDamageByEntityEvent){
+            $damage = $event->getDamager();
+            if($damage instanceof Player){
+                if($damage->getInventory()->getItemInHand()->getItem() === 367){
+                    $player->sendTip("Celeron activated");
+                }
+            }
+        }
+    }
 }
-
